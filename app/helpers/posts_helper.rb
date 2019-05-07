289b394cf31687af7de9aef92d42157c31f48058
bebopvinh <vinhdo07@gmail.com>
2019-05-06 21:31:49 -0700

    Done.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,9 @@
 module PostsHelper
-  
+   def author_id_field(post)
+      if post.author.nil?
+         collection_select(:post, :author_id, Author.all, :id, :name)
+      else
+         hidden_field(:post, :author_id) 
+      end
+   end
 end
